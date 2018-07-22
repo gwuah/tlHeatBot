e32935716cd1f781e2b177e73808e00ed3dec19c
gwuah <griffith@dolphtech.com>
2018-07-22 20:34:24 +0000

    add configjs

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  keys: {
+    consumer_key: "",
+    consumer_secret: "",
+    access_token: '',
+    access_token_secret: ''
+  },
+  
+  me: { 
+    screen_name: "oh_promi",
+    user_id: '949741141548814336'
+  },
+    
+  griff: {
+    screen_name: "gwuah_",
+    user_id: '3364921872'
+  }
+}
